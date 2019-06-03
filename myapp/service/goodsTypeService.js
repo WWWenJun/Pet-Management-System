e3c11cb32720dad8f101e96f126c1a235eed0def
DESKTOP-1C9C67S\Xue <760947868@qq.com>
2019-06-03 17:23:59 +0800

    no message

--- myapp/service/goodsTypeService.js
+++
@@ -1,9 +0,0 @@
-const { addGoodsType,getGoodsType } = require("../dao/goodsTypeDao.js");
-
-module.exports.addGoodsType = async data => {
-    await addGoodsType(data);
-    return true;
-}
-module.exports.getGoodsType = async () => {
-    return await getGoodsType();
-}
